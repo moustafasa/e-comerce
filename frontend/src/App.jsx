77a67288df78da0b1ece9f89c925251f3e405403
moustafasa <moustafasaad954@gmail.com>
2024-01-16 02:51:29 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PropTypes from "prop-types";
+
+function App({ test }) {
+  const [count, setCount] = useState(0);
+  return <>test</>;
+}
+
+export default App;
